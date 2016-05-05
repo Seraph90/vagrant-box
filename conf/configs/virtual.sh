f933bc02fd7bda55e720cf7306b80d2398beb010
Logvinov Andrey <lai@ag-media.ru>
2016-05-05 10:33:09 +0300

    Небольшие изменения в скриптах.

--- conf/configs/virtual.sh
+++ conf/configs/virtual.sh
@@ -2,4 +2,6 @@
 
 echo "Create symlinks"
 	rm /etc/nginx/sites-enabled/akbenergo.conf
+	rm /etc/nginx/sites-enabled/wordpress.conf
     ln -s /etc/nginx/sites-available/akbenergo.conf /etc/nginx/sites-enabled/
+    ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
